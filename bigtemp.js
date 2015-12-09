f7e94d4089591c2210acc4f894c7e14f3df4062f
Adam Chao <adam@thinkaliker.com>
2015-12-08 18:14:56 -0800

    change everything, cached database, firebase, package.json, cold override

--- bigtemp.js
+++ bigtemp.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var temp = 9999;
   var apicheck;
+  var chill;
 
   if (localStorage.getItem('api') === null || localStorage.getItem('api') == "null") {
     apicheck = false;
@@ -53,31 +54,6 @@ $(document).ready(function() {
     }
   });
 
-  function updateC() {
-    $("#f").removeClass("btn-success");
-    $("#f").addClass("btn-danger");
-    $("#c").removeClass("btn-danger");
-    $("#c").addClass("btn-success");
-  }
-
-  function updateF() {
-    $("#c").removeClass("btn-success");
-    $("#c").addClass("btn-danger");
-    $("#f").removeClass("btn-danger");
-    $("#f").addClass("btn-success");
-  }
-
-  function updateTemp(curTemp) {
-    temp = curTemp;
-    if (localStorage.getItem('fc') == "true") {
-      $('#temp').html(temp + '&deg;F');
-    } else {
-      //console.log(">mfw not american");
-      $('#temp').html(Math.round((temp - 32) * (5 / 9)) + '&deg;C');
-    }
-  }
-
-
   $("#close").click(function() {
     $('#settings').modal('hide');
   });
@@ -93,6 +69,7 @@ $(document).ready(function() {
     var lat = crd.latitude;
     var lon = crd.longitude;
     var url = 'http://node.thinkaliker.com:8888/?loc=' + lat + ',' + lon + "&callback=?";
+    //var url = 'http://localhost:8888/?loc=' + lat + ',' + lon + "&callback=?";
     var urlapi = 'https://api.forecast.io/forecast/' + localStorage.getItem('api') + '/' + lat + ',' + lon + "?callback=?";
 
     if (apicheck) {
@@ -103,11 +80,17 @@ $(document).ready(function() {
       });
     } else {
       $.getJSON(url, function(data) {
+        if (data.hasOwnProperty('html')) {
+          console.log("c h i l l");
+          chill = data.html;
+          $("body").append(chill);
+        } else {
+          $("body").prepend('<a id="notcold" >not cold enough?</a>')
+        }
         console.log("Node " + lat + ", " + lon + " : " + data.temp);
         updateTemp(data.temp);
       });
     }
-
   }
 
   function showError(error) {
@@ -127,4 +110,50 @@ $(document).ready(function() {
     }
     $('#temp').text("N/A");
   }
+
+ $("body").on('click', "#notcold", function() {
+    //somewhere in canada
+    var lat = 71.985400;
+    var lon = -125.236280;
+    var url = 'http://node.thinkaliker.com:8888/?loc=' + lat + ',' + lon + "&callback=?";
+   //var url = 'http://localhost:8888?loc=' + lat + ',' + lon + "&callback=?";
+    console.log("not cold enough, eh?");
+    $.getJSON(url, function(data) {
+      chill = data.html;
+      $("body").append(chill);
+      console.log("COLD " + lat + ", " + lon + " : " + data.temp);
+      updateTemp(data.temp);
+    });
+  });
+
 });
+
+function clamp(num, min, max) {
+  return num < min ? min : num > max ? max : num;
+}
+
+function updateTemp(curTemp) {
+  temp = curTemp;
+  if (localStorage.getItem('fc') == "true") {
+    $('#temp').html(temp + '&deg;F');
+  } else {
+    //console.log(">mfw not american");
+    $('#temp').html(Math.round((temp - 32) * (5 / 9)) + '&deg;C');
+  }
+  var hue = 4 * clamp(temp, 50, 90) - 10;
+  $("body").css("background-color", "hsl(" + hue + ", 50%, 50% )");
+}
+
+function updateC() {
+  $("#f").removeClass("btn-success");
+  $("#f").addClass("btn-danger");
+  $("#c").removeClass("btn-danger");
+  $("#c").addClass("btn-success");
+}
+
+function updateF() {
+  $("#c").removeClass("btn-success");
+  $("#c").addClass("btn-danger");
+  $("#f").removeClass("btn-danger");
+  $("#f").addClass("btn-success");
+}
--- nodeweather.js
+++ nodeweather.js
@@ -3,7 +3,14 @@ var url = require("url");
 var fs = require("fs");
 var https = require("https");
 var request = require("request");
+var firebase = require("firebase");
+var timestamp = require('log-timestamp');
+var ref = new firebase("https://radiant-torch-9623.firebaseio.com/");
 var api;
+var bing;
+var load;
+
+console.log("Server started!");
 
 //Provide your own API key
 fs.readFile('./api.key', 'utf8', function(err, data) {
@@ -13,38 +20,153 @@ fs.readFile('./api.key', 'utf8', function(err, data) {
   api = data;
 });
 
-http.createServer(function(req, res) {
-  var query = url.parse(req.url, true).query;
+fs.readFile('./bing.key', 'utf8', function(err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  bing = data;
+});
 
-  loc = query.loc;
-  var temperature;
+fs.readFile('./load.html', 'utf8', function(err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  load = data;
+});
 
+function fetchPostalCode(callback, location, bing) {
+  //fetch postal code from Bing
+  //https://msdn.microsoft.com/en-us/library/ff701710.aspx
+  var bingurl = 'http://dev.virtualearth.net/REST/v1/Locations/' + location + '?key=' + bing;
+  request(bingurl, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      try{
+        var bingdata = JSON.parse(body);
+      } catch (e) {
+        return console.error(e);
+      }
+      console.log(bingdata);
+
+      var parse2 = JSON.stringify(bingdata);
+      var parse = JSON.parse(parse2);
+
+      //console.log(parse);
+      console.log(parse.resourceSets[0]);
+      var postal = parse.resourceSets[0].resources[0].address.postalCode;
+
+      //      console.log("POSTAL: " + postal);
+    } else {
+      console.log("LOC: " + location + " // error retrieving postal");
+      postal = "00000"
+    }
+    callback(postal);
+  });
+}
+
+function fetchForecast(callback, location, tempref, foundpostal) {
   //Fetch from forecast.io
-  var fetchurl = 'https://api.forecast.io/forecast/' + api + '/' + loc;
+  var fetchurl = 'https://api.forecast.io/forecast/' + api + '/' + location;
   request(fetchurl, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
+
+      //set firebase
+      var time = new Date().getTime();
+      data.timestamp = time;
+      //console.log(data);
+      var tempobj = {};
+      tempobj[foundpostal] = data;
+      tempref.child(foundpostal).set(data);
+
       temperature = (Math.round(data.currently.temperature));
-      console.log(loc + " : " + temperature);
+      console.log(location + " : " + temperature);
     } else {
       temperature = "N/A";
     }
+    callback(temperature);
+  });
+}
 
-    var json = JSON.stringify({
+function fetchFirebase(callback, postal, ref, location) {
+  temp = ref.child("" + postal + "");
+  temp.once('value', function(snapshot) {
+      //https://radiant-torch-9623.firebaseio.com/
+      var currTime = new Date().getTime();
+      if (currTime - snapshot.child("timestamp").val() > 3600000) {
+        console.log("update weather info " + snapshot.child("timestamp").val());
+        fetchForecast(function(temperature){
+          callback(temperature)
+        } ,location, ref, postal);
+
+      } else {
+
+        var temperature = Math.round(snapshot.child("currently").child("temperature").val());
+        //console.log("snapshot " + snapval);
+        //var temperature = snapshot.currently.temperature;
+        console.log(postal + " : " + location + " : " + temperature);
+        callback(temperature);
+      }
+
+    },
+    function(err) {
+      console.log("err " + err);
+      callback("ERR");
+    }
+  );
+
+}
+
+function writeJSON(temperature, res, query) {
+  var json;
+  if (temperature > "55") {
+    json = JSON.stringify({
       "temp": temperature
     });
+  } else {
+    var html = load;
+    json = JSON.stringify({
+      "temp": temperature,
+      "html": html
+    })
+  }
 
-    res.writeHead(200, {
-      "Content-Type": "application/json"
-    });
-
-    if (query.callback) {
-      res.write(query.callback + '(' + json + ');');
-    } else {
-      res.write(json);
-    }
-    res.end();
-
+  res.writeHead(200, {
+    "Content-Type": "application/json"
   });
+
+  if (query.callback) {
+    res.write(query.callback + '(' + json + ');');
+  } else {
+    res.write(json);
+  }
+  res.end();
+}
+
+http.createServer(function(req, res) {
+  var query = url.parse(req.url, true).query;
+  location = query.loc;
+  var tempref = ref;
+
+  fetchPostalCode(function(foundpostal) {
+    tempref.once('value', function(snapshot) {
+      if (!snapshot.hasChild(foundpostal)) {
+
+        fetchForecast(function(temperature) {
+          writeJSON(temperature, res, query);
+        }, location, tempref, foundpostal);
+
+      } else {
+        var postalref = tempref + "/" + foundpostal;
+        console.log("POSTAL: " + foundpostal + " exists already");
+
+        fetchFirebase(function(temperature) {
+          writeJSON(temperature, res, query);
+        }, foundpostal, tempref, location);
+
+      }
+
+    });
+  }, location, bing);
+
 
 }).listen(8888);
